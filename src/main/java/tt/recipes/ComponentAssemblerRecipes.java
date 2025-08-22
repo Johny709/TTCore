65ff494f20bf557dc0ef6a810323eb3f265ec2c7
Johny709 <janjoseph.merida@gmail.com>
2025-08-22 15:18:10 -0400

    fix robot arms in component assembler recipes

--- src/main/java/tt/recipes/ComponentAssemblerRecipes.java
+++ src/main/java/tt/recipes/ComponentAssemblerRecipes.java
@@ -1,8 +1,33 @@
 package tt.recipes;
 
+import gregtech.api.recipes.RecipeMap;
+import gregtech.api.unification.ore.OrePrefix;
+import gregtech.common.items.MetaItems;
+
+import static gregtech.api.unification.material.MarkerMaterials.Tier.LV;
+import static gregtech.api.unification.material.Materials.Steel;
+import static gregtech.api.unification.material.Materials.Tin;
+
 public class ComponentAssemblerRecipes {
 
     public static void init() {
+        RecipeMap<?> STEAM_COMPONENT_ASSEMBLER_RECIPES = RecipeMap.getByName("steam_component_assembler");
+        RecipeMap<?> COMPONENT_ASSEMBLER_RECIPES = RecipeMap.getByName("electric_component_assembler");
 
+        STEAM_COMPONENT_ASSEMBLER_RECIPES.recipeBuilder()
+                .input(OrePrefix.cableGtSingle, Tin, 3)
+                .input(OrePrefix.stick, Steel, 2)
+                .input(OrePrefix.circuit, LV, 1)
+                .inputs(MetaItems.ELECTRIC_MOTOR_LV.getStackForm(), MetaItems.ELECTRIC_PISTON_LV.getStackForm())
+                .duration(100).EUt(8)
+                .buildAndRegister();
+
+        COMPONENT_ASSEMBLER_RECIPES.recipeBuilder()
+                .input(OrePrefix.cableGtSingle, Tin, 3)
+                .input(OrePrefix.stick, Steel, 2)
+                .input(OrePrefix.circuit, LV, 1)
+                .inputs(MetaItems.ELECTRIC_MOTOR_LV.getStackForm(), MetaItems.ELECTRIC_PISTON_LV.getStackForm())
+                .duration(100).EUt(30)
+                .buildAndRegister();
     }
 }
