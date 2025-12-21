5129522193d12173e111b769763821636a9bb6c5
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-12-21 12:19:19 -0500

    change mod name to TTCore

--- src/main/java/tt/TTCore.java
+++ src/main/java/tt/TTCore.java
@@ -6,10 +6,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-@Mod(modid = TTTweaks.MOD_ID, name = TTTweaks.MOD_NAME, version = TTTweaks.VERSION, acceptableRemoteVersions = "*")
-public class TTTweaks {
-    public static final String MOD_ID = "tttweaks";
-    public static final String MOD_NAME = "TTTweaks";
+@Mod(modid = TTCore.MOD_ID, name = TTCore.MOD_NAME, version = TTCore.VERSION)
+public class TTCore {
+    public static final String MOD_ID = "ttcore";
+    public static final String MOD_NAME = "TTCore";
     public static final String VERSION = "1.0";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
